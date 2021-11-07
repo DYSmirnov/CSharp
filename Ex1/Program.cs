e01611c5054c3e24dbd962b4e46c86b0ee512d7e
Администратор <@WIN-GMAKK9MSC73>
2021-11-07 18:47:49 +0300

    Revert "Revert "d""
    This reverts commit 11c862cf326b95f1de039b5301d599aa2f0e37d1.

---
+++ Ex1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = GetDigFromUser();
+            SimpleNumbers(num);
+        }
+        
+        static int GetDigFromUser()
+        {
+            Console.WriteLine("Enter digit");
+            int dig = int.Parse(Console.ReadLine());
+            return dig;
+        }
+
+        static void SimpleNumbers(int numb)
+         {
+            int t_f;
+            t_f = 1;
+            if (numb > 0 & numb <3)
+                t_f = 1; 
+            else if (numb < 1)
+                t_f = 0;
+                
+            else
+            for (int i = 2; i < numb; i++) 
+            {
+                if (numb % i == 0)
+                    t_f = 2;
+
+            }
+            
+            Res(t_f);
+        }
+        static void Res(int tf)
+        {
+            if (tf == 0)
+                Console.WriteLine("Incorrect digit");
+            else if (tf == 1)
+                Console.WriteLine("This is simple digit");
+            else
+                Console.WriteLine("This is not simple digit");
+        }
+    }
+}
